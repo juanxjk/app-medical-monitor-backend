e79e4aa680d27b6f660eba4ea5106688592faa4d
Juan Carlos <juanksoliveira@gmail.com>
2021-04-01 04:10:58 -0300

    feat: add jwt authentication middleware

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,10 +6,15 @@ dotenv.config({
 
 type AppConfigType = {
   port: number;
+  secret: string;
   bcryptRounds: number;
 };
+
+if (!process.env.JWTSECRET) throw new Error("[ERROR] JWTSECRET is not defined");
+
 const appConfig: AppConfigType = {
   port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  secret: process.env.JWTSECRET,
   bcryptRounds: 10,
 };
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
+
+import { auth } from "./middlewares/auth";
+
 const routes = Router();
 
 // PUBLIC
 routes.get("/", (req, res) => {
   res.send({ message: "It works! 🎉" });
 });
+routes.post("/sessions", SessionController.login);
+
+// AUTHENTICATED
+
+routes.use(auth);
 
 export default routes;
