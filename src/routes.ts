6599a7e384811026e33d74fd859eb5fba0570887
Juan Carlos <juanksoliveira@gmail.com>
2021-04-01 04:11:46 -0300

    feat: add routes to controllers

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,11 @@ import { Router } from "express";
 
 import { auth } from "./middlewares/auth";
 
+import UserController from "./controllers/UserController";
+import SessionController from "./controllers/SessionController";
+import DeviceController from "./controllers/DeviceController";
+import PatientController from "./controllers/PatientController";
+
 const routes = Router();
 
 // PUBLIC
@@ -13,5 +18,23 @@ routes.post("/sessions", SessionController.login);
 // AUTHENTICATED
 
 routes.use(auth);
+
+routes.get("/devices", DeviceController.index);
+routes.get("/devices/:id", DeviceController.show);
+routes.post("/devices", DeviceController.create);
+routes.patch("/devices/:id", DeviceController.update);
+routes.delete("/devices/:id", DeviceController.delete);
+
+routes.get("/patients", PatientController.index);
+routes.get("/patients/:id", PatientController.show);
+routes.post("/patients", PatientController.create);
+routes.patch("/patients/:id", PatientController.update);
+routes.delete("/patients/:id", PatientController.delete);
+
+routes.get("/users", UserController.index);
+routes.get("/users/:id", UserController.show);
+routes.post("/users", UserController.create);
+routes.patch("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.delete);
 
 export default routes;
